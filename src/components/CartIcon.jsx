7fec1e18f799ad8678a70f668528a50a2550b387
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-12-22 10:37:10 -0500

    represent state in navbar

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,13 +1,13 @@
 import { TiShoppingCart } from "react-icons/ti";
 
 
-export default function CartIcon(){
+export default function CartIcon({ cartQuantity }){
 
     return(
         <>
             <div id="cart-icon-wrap">
                 <TiShoppingCart style={{ fontSize: "2rem" }} />
-                <p id="number-of-items">0</p>
+                <p id="number-of-items">{cartQuantity}</p>
             </div>
             <button id="checkout-btn">Check Out</button>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,37 @@
-import { Link } from "react-router-dom"
 import { NavLink } from "react-router-dom"
-export default function Navbar(){
+import CartIcon from "./CartIcon";
+
+export default function Navbar({ cartQuantity }){
+
 
     return(
         <div id="navbar">
             <div id="logo">
                 Cart.
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to={"/"}
-                        className={({ isActive }) => isActive ? "active" : ""}
-                        viewTransition>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={"/shop"}
-                        className={({ isActive }) => isActive ? "active" : ""}
-                        viewTransition>
-                            Shop
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <div id="navbar-second-child">
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to={"/"}
+                            className={({ isActive }) => isActive ? "active" : ""}
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/shop"}
+                            className={({ isActive }) => isActive ? "active" : ""}
+                            >
+                                Shop
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div id="shop-wrap">
+                    <CartIcon cartQuantity={cartQuantity} />
+                </div>
+            </div>
         </div>
     )
 }
