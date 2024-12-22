4f6f5feedd7dd8d7f1cc495e48865e20a5071dc0
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-12-22 10:39:54 -0500

    write functions to alter state on button click

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,6 +1,19 @@
+import { useOutletContext } from "react-router-dom";
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 
 export default function ItemCard({ product }){
+
+    const [ cartQuantity, setCartQuantity ] = useOutletContext();
+
+    function addToCart(){
+
+        setCartQuantity(cartQuantity + 1);
+    }
+
+    function removeFromCart(){
+
+        if(cartQuantity > 0) setCartQuantity(cartQuantity - 1);
+    }
 
     return(
         <div className="item-card">
@@ -9,8 +22,8 @@ export default function ItemCard({ product }){
             </div>
             <h3>{product.title.slice(0, 30) + "..."}</h3>
             <div id="btns-wrap">
-                <button>Add to cart</button>
-                <button><MdOutlineRemoveShoppingCart /></button>
+                <button onClick={addToCart}>Add to cart</button>
+                <button onClick={removeFromCart}><MdOutlineRemoveShoppingCart /></button>
             </div>
             <p>{"$" + product.price}</p>
         </div>
