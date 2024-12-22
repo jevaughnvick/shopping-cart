1de4563b6907846787b1169cee9cb5695e4328f6
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-12-21 20:06:11 -0500

    refactor app scaffolding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "../routes";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import NotFound from "../pages/NotFound"
+import Layout from "./Layout";
 
-const router = createBrowserRouter(routes);
+
+const router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFound />,
+        children: [
+            {index: true, element: <Home />},
+            {path: "/shop", element: <Shop />},
+        ]
+    }
+]);
 
 export default function App(){
 
-    return <RouterProvider router={router}/>
+    return(
+        <>
+            <RouterProvider router={router}/>
+        </>
+    )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
+// import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
-export default function Layout({ children }){
 
-return (
+export default function Layout(){
+
+    return (    
     <>
         <Navbar />
-        <main>{children}</main>
+        <main>
+            <Outlet />
+        </main>
         <Footer />
     </>
-)
+    )
 }
