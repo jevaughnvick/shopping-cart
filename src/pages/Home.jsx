8817fa3d2f159a7046906278916ee479511131c3
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-12-23 13:05:01 -0500

    make home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,16 @@ export default function Home(){
         <>
             <div id="hero">
                 <div>
-                    <h1>Get <span>Nothing</span><br></br>from Cart</h1>
+                    <h1>Get<br></br><span>Nothing</span><br></br>from Cart</h1>
                     <p>Cause it's fake.</p>
                 </div>
-                <img src="/src/assets/fakeremove.png" alt="Fake store" />
+                <div>
+                    <img src="/src/assets/fakeremove.png" alt="Fake store" />
+                </div>
             </div>
             <section id="main-section">
                 <h2>Cart is fake.</h2>
-                <p>Lorem fake dolor sit fake consectetur adipisicing elit. fake quod dolore rem, dolorum fake eaque totam maxime corrupti ipsa fake? Voluptas, dolor, debitis magnam nulla voluptates fake tenetur, porro fake veniam deserunt iusto magni? fake molestiae fugit totam nam nemo fugiat ipsa ab fake tenetur molestias non porro perferendis accusamus, mollitia velit, sint fake ducimus atque pariatur!</p>
+                <p>Lorem fake dolor sit fake consectetur adipisicing elit. fake quod dolore rem, dolorum fake eaque totam maxime corrupti ipsa fake? Voluptas, dolor, debitis magnam nulla voluptates fake tenetur, porro fake veniam deserunt iusto magni?</p>
             </section>
         </>
     )   
